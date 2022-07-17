ef4a3ec2fcd6a11f3ae8f2a64722df988bdaded9
DimaMorochenets <morochenets.dm@gmail.com>
2022-07-17 19:31:10 +0300

    Fix build script for windows

--- ImageDownloader/build_windows.bat
+++ ImageDownloader/build_windows.bat
@@ -13,8 +13,12 @@ if exist %build_path% (
     cmake -B %build_path%
 )
 
-cmake --build %build_path%
+cmake --build %build_path% --parallel %NUMBER_OF_PROCESSORS%
 
-echo Run application: (y/n)?
-set /p answer=""
-if "%answer%" == "y" ("%build_path%\Debug\ImageDownloader.exe")
+::echo Run application: (y/n)?
+::set /p answer=""
+::if "%answer%" == "y" ("%build_path%\Debug\ImageDownloader.exe")
+
+C:\Qt\6.3.1\msvc2019_64\bin\windeployqt.exe --qmldir . --release "%build_path%\Debug\ImageDownloader.exe"
+
+"%build_path%\Debug\ImageDownloader.exe"
